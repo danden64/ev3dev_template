f4e5abdac576bd7015a5be7d65473e617abe4085
Danil Yahnov <35452498+danden64@users.noreply.github.com>
2023-05-17 12:36:15 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 import struct, time
+from threading import Thread
+
+global lx, ly, rx, ry, bX, bTriangle, bSquare, bCircle
+global RUNNING
+RUNNING = True
 
 # Scaling function for converting stick values (0,255) to (-100,100)
 def scale(val, src, dst):
@@ -16,50 +21,91 @@ def scale(val, src, dst):
 ev3 = EV3Brick()
 ev3.speaker.beep()
 
-# Declare motor ports
-left_motor = Motor(Port.B)
-right_motor = Motor(Port.C)
+# Initialise motors
+b = Motor(Port.B)
+c = Motor(Port.C)
 
-# Declare variables
-drive = 0
-turn = 0
+# Autonomous
+def Auto():
+    ev3.say("Autonomous")
+
+start_time = time.time()
+while start_time < start_time + 15:
+    Auto()
+
+# Drive motors
+def mainThread():
+    global RUNNING
+    global lx, ly, rx, ry, bX, bTriangle, bSquare, bCircle
+
+    while RUNNING:
+
+        b_speed = ly - rx
+        c_speed = ly + rx
+
+        b.dc(b_speed)
+        c.dc(c_speed)
+        
+thread = Thread(target=mainThread)
+thread.start()
 
 # Setup for reading controller input
 infile_path = "/dev/input/event4"
-in_file = open(infile_path, "rb")
-FORMAT = 'llHHI'
-EVENT_SIZE = struct.calcsize(FORMAT)
-event = in_file.read(EVENT_SIZE)
-
-# Main loop
-while event:
-    
-    # Read incoming event
-    (tv_sec, tv_usec, ev_type, code, value) = struct.unpack(FORMAT, event)
-
-    # Joystick moved
-    if ev_type == 3:
-
-        if code == 0: # Left stick x
-            pass
-            
-        if code == 1: # Left stick y
-            drive = scale(value, (0,255), (-100,100))
-
-        if code == 3: # Right stick x
-            turn = scale(value, (0,255), (-100,100))
-
-        if code == 4: # Right stick y
-            pass
-
-    # Set speeds/directions and drive motors
-    left_speed = drive - turn
-    right_speed = drive + turn
-
-    left_motor.dc(left_speed)
-    right_motor.dc(right_speed)
-
-    # Read new event
+with open(infile_path, "rb") as in_file:
+    FORMAT = 'llHHI'
+    EVENT_SIZE = struct.calcsize(FORMAT)
     event = in_file.read(EVENT_SIZE)
 
-in_file.close()
+    # Main loop
+    while event:
+        
+        # Read incoming event
+        (tv_sec, tv_usec, ev_type, code, value) = struct.unpack(FORMAT, event)
+
+        # Joystick moved
+        if ev_type == 3:
+
+            if code == 0: # Left stick x
+                lx = scale(value, (0,255), (-100,100))
+                
+            if code == 1: # Left stick y
+                ly = scale(value, (0,255), (-100,100))
+
+            if code == 3: # Right stick x
+                rx = scale(value, (0,255), (-100,100))
+
+            if code == 4: # Right stick y
+                ry = scale(value, (0,255), (-100,100))
+
+        # Button pressed
+		if ev_type == 1:
+
+			if code == 304: # X button
+				if value == 1:
+                    bX = True
+                elif value == 0:
+                    bX = False
+
+			if code == 305: # O button
+				if value == 1:
+                    bCircle = True
+                elif value == 0:
+                    bCircle = False
+			
+			if code == 307: # △ button
+				if value == 1:
+                    bTriangle = True
+                elif value == 0:
+                    bTriangle = False
+
+			if code == 308: # □ button
+				if value == 1:
+                    bSquare = True
+                elif value == 0:
+                    bSquare = False
+
+        # Read new event
+        event = in_file.read(EVENT_SIZE)
+
+RUNNING = False
+ev3.speaker.beep()
